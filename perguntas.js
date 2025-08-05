8cf9f920afa2d2a7e117d82317de390b5ef832fd
KAROL2008-JP <karolyne.alcantara@escola.pr.gov.br>
2025-08-05 11:33:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação mobile',
+    'Qual a diferença entre app nativo e híbrido?',
+    'O nativo é feito para um sistema específico; Híbrido roda em vários com o mesmo código'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'A ÁSIA'
+)
+
+criaCartao(
+    'Programação',
+    'O que é html?',
+    'É a linguagem usada para estruturar páginas na web'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz beijo em inglês?',
+    'beijo em ingles é kiss (beijo)'
+)
